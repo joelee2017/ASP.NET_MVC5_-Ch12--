964957999f5ca57d69888fcf19fc4bf1ec898d89
joe lee <as955133@gmail.com>
2018-07-03 20:31:27 +0800

    加入專案檔案。

---
+++ ASP.NET_MVC5_-Ch12-身份驗證與授權/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC5__Ch12_身份驗證與授權
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP.NET_MVC5_-Ch12-身份驗證與授權/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC5__Ch12_身份驗證與授權.MvcApplication" Language="C#" %>
